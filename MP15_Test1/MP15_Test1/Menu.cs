971c166d696716346e18c48d7098c6f7c5700b2a
YeSeul <ths0114@naver.com>
2026-08-28 10:42:21 +0900

    [Feat] 메뉴 클래스 생성

--- MP15_Test1/MP15_Test1/Menu.cs
+++ MP15_Test1/MP15_Test1/Menu.cs
@@ -1,6 +1,19 @@
 ﻿namespace MP15_Test1;
 
-public class Menu
+public abstract class Menu
 {
+    // 멤버
+    public string MenuName;
+    public string MenuType;
+    protected int Price { get; set; }
+
+    // 생성자
+    public Menu(string menuName, string menuType, int price)
+    {
+        MenuName = menuName;
+        MenuType = menuType;
+        Price = price;
+    }
+    
     
 }
--- MP15_Test1/MP15_Test1/Program.cs
+++ MP15_Test1/MP15_Test1/Program.cs
@@ -1,14 +1,22 @@
 ﻿using  System;
-
 class Program
 {
-        public const string CAFE_NAME = "무인 아이스크림 1호점";
+        public const string STOR_NAME = "무인 아이스크림 1호점";
         public const int DISCOUNT = 3;
         public const float DISCOUNT_RATE = 0.2f;
-        
-        static void Main(string[] args)
+
+        void Main(string[] args)
         {
-            
-            
+                Line();
+                Console.WriteLine($"[{STOR_NAME}] 주문 키오스크");
+                Line();
+                Console.WriteLine("[구매하실 상품을 선택하세요.]");
+
+        }
+
+        public void Line()
+        {
+                Console.WriteLine("====================================");
         }
 }
+
