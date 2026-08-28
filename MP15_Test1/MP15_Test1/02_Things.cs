ec7eeaaad340210315a0b223339416339182565d
YeSeul <ths0114@naver.com>
2026-08-28 11:35:01 +0900

    [Feat] 할인 메서드 생성

--- MP15_Test1/MP15_Test1/02_Things.cs
+++ MP15_Test1/MP15_Test1/02_Things.cs
@@ -21,7 +21,25 @@ public class Things : Menu
         new Things("죠리뽕",(Type)2, 1700),
         new Things("꼬북칩",(Type)2, 1700),
     };
-    
-    
-    
+
+    public static int DiscountPrice(int buyCount,int price,Type type, float DiscountRate)
+    {
+        int totalDiscount = 0;
+        if (buyCount >= 5) // 5개 이상 사면 20% 할인
+        {
+            totalDiscount = (int)(buyCount * price * DiscountRate);
+        }
+        else if (type == Type.Snack && buyCount >= 2) // 과자 2+1 행사
+        {
+            int free = 0;
+            free = buyCount / 2;
+            totalDiscount = free * 1700;
+        }
+        else
+        {
+        }
+
+        return totalDiscount;
+    }
+
 }
